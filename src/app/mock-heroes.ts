f6f26b2bff8945aa124b7d6c6a662b6917c7fdfa
shreikgristle <116908489+youngj3@users.noreply.github.com>
2023-04-24 15:31:33 -0400

    "Create a feature component"

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'You know our love was meant to be' },
+  { id: 13, name: 'The kind of love that lasts forever' },
+  { id: 14, name: 'And I want you here with me' },
+  { id: 15, name: 'From tonight until the end of time' },
+  { id: 16, name: 'You should know, everywhere I go' },
+  { id: 17, name: 'Always on my mind, in my heart' },
+  { id: 18, name: 'In my soul, baby' },
+  { id: 19, name: "You're the meaning in my life" },
+  { id: 20, name: "You're the inspiration" }
+];
