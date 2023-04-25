0c963983a08a8cfc5d8395d346f481fd47eb422b
shreikgristle <116908489+youngj3@users.noreply.github.com>
2023-04-25 11:31:32 -0400

    added services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = [];
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
@@ -26,5 +27,5 @@ export class HeroesComponent {
     this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 }
